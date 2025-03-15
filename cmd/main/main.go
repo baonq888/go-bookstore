b470923eb2b8991562dabc1a7eaddb2360068e0f
Nguyen Quoc Bao <qbhoalu@gmail.com>
2025-03-15 20:23:10 +0700

    fix bug

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"go-bookstore/pkg/routes"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
 	_ "gorm.io/driver/postgres"
-	"go-bookstore/pkg/routes"
 )
 
 func main() {
+
 	r := mux.NewRouter()
 	routes.RegisterBookStoreRoutes(r)
 	http.Handle("/",r)
+	fmt.Println("Server running on port 9010")
 	log.Fatal(http.ListenAndServe("localhost:9010",r))
+	
 }
